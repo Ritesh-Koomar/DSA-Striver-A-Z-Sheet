088dc47cca92be2dab2f2f52fc16b2be16899ef3
Ritesh-Koomar <139606742+Ritesh-Koomar@users.noreply.github.com>
2026-03-30 20:24:11 +0530

    Create _7_CountOccurencesinSortedArray.java

---
+++ Binary Search/_7_CountOccurencesinSortedArray.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        int fo= -1;
+        int lo = -1;
+        while(s<=e)
+        {
+            int m = s + (e - s)/2;
+            if(nums[m] == target)
+            {
+                fo = m;
+                e = m -1;
+            }
+            else if(nums[m] > target)
+            e = m - 1;
+            else
+            s = m + 1;
+        }
+        s = 0;
+        e = nums.length - 1;
+
+        while(s<=e)
+        {
+            int m = s + (e - s)/2;
+            if(nums[m] == target)
+            {
+                lo = m;
+                s = m + 1;
+            }
+            else if(nums[m] > target)
+            e = m - 1;
+            else
+            s = m + 1;
+        }
+        return lo - fo + 1;
+    }
+}
